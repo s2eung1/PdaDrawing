a2269b2e74ba625417bc89dd2df2ab8679cf2690
cksl0830 <dkssudgkdl9@naver.com>
2021-11-29 16:45:41 +0900

    backend final
    final -b

---
+++ DB/create.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS linedraw;
+CREATE DATABASE linedraw DEFAULT CHARSET=utf8 COLLATE=UTF8_BIN;
+
+USE linedraw;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id int(10) NOT NULL AUTO_INCREMENT,
+    name varchar(25) NOT NULL,
+    email varchar(40) NOT NULL,
+    pw varchar(20) NOT NULL,
+    PRIMARY KEY(id, email)
+);
+
+DROP TABLE IF EXISTS image;
+CREATE TABLE image(
+    id int(10) NOT NULL AUTO_INCREMENT,
+    users_id int(10) NOT NULL,
+    bucket_link varchar(50) NOT NULL,
+    date date NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(users_id) REFERENCES users(id) on update cascade on delete cascade
+);
+
